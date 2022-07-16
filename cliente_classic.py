ea6adedd4558e0c3a388f4d307680feca5a9e43f
R. Nicolás Savinelli <savinelli@ieee.org>
2022-07-16 15:12:36 -0300

    set standard for cliente class and add cliente_black

--- cliente_classic.py
+++
@@ -1,8 +0,0 @@
-# importar cliente dependiendo donde este from cliente import Cliente
-# importar algun tipo de evento / razon
-class ClienteClassic (Cliente):
-    def __init__(self, **kwargs) -> None:
-        super(ClienteClassic, self).__init__(**kwargs)
-
-        def get_razon_compra_cuotas_visa(self, evento: Evento):
-            return "Este cliente no puede tener tarjetas de credito"
---
+++ src/pkg/cliente/__init__.py
@@ -0,0 +1,181 @@
+from ..cuenta import Cuenta, LIMITE_MAXAXIMO_DE_TRANSFERENCIA
+from ..direccion import Direccion
+
+
+CLASSIC = "CLASSIC"
+GOLD = "GOLD"
+BLACK = "BLACK"
+
+
+class Cliente:
+    cuentas: list[Cuenta]
+
+    direccion: Direccion
+
+    nombre: str
+    apellido: str
+    numero: str
+    dni: str
+
+    def __init__(
+        self,
+        cuentas: list[Cuenta],
+        direccion: Direccion,
+        nombre: str,
+        apellido: str,
+        numero: str,
+        dni: str,
+    ) -> None:
+        self.cuentas = cuentas
+        self.direccion = direccion
+        self.nombre = nombre
+        self.apellido = apellido
+        self.numero = numero
+        self.dni = dni
+
+    def puede_crear_chequera(self) -> bool:
+        return False
+
+    def puede_crear_tarjeta_credito(self) -> bool:
+        return False
+
+    def puede_comprar_dolar(self) -> bool:
+        return False
+
+
+class ClienteClassic(Cliente):
+    def __init__(
+        self,
+        direccion: Direccion,
+        nombre: str,
+        apellido: str,
+        numero: str,
+        dni: str,
+    ) -> None:
+        cuentas: list[Cuenta] = []
+
+        caja_de_ahorro_en_pesos: Cuenta = Cuenta(
+            limite_extraccion_diario=10000,
+            limite_trasnferencia_recibida=150000,
+            monto=0,
+            costo_trasnferencia=0.01,
+            saldo_descubierto_disponible=0,
+        )
+
+        cuentas.append(caja_de_ahorro_en_pesos)
+
+        super().__init__(
+            cuentas=cuentas,
+            direccion=direccion,
+            nombre=nombre,
+            apellido=apellido,
+            numero=numero,
+            dni=dni,
+        )
+
+    def puede_crear_chequera(self) -> bool:
+        return False
+
+    def puede_crear_tarjeta_credito(self) -> bool:
+        return False
+
+    def puede_comprar_dolar(self) -> bool:
+        return False
+
+
+class ClienteGold(Cliente):
+    def __init__(
+        self,
+        direccion: Direccion,
+        nombre: str,
+        apellido: str,
+        numero: str,
+        dni: str,
+    ) -> None:
+        cuentas: list[Cuenta] = []
+
+        cuenta_corriente: Cuenta = Cuenta(
+            limite_extraccion_diario=10000,
+            limite_trasnferencia_recibida=500000,
+            monto=0,
+            costo_trasnferencia=0.05,
+            saldo_descubierto_disponible=10000,
+        )
+
+        caja_de_ahorro_en_dolares: Cuenta = Cuenta(0, 0, 0, 0, 0,)
+
+        cuentas.append(cuenta_corriente)
+
+        cuentas.append(caja_de_ahorro_en_dolares)
+
+        super().__init__(
+            cuentas=cuentas,
+            direccion=direccion,
+            nombre=nombre,
+            apellido=apellido,
+            numero=numero,
+            dni=dni,
+        )
+
+    def puede_crear_chequera(self) -> bool:
+        return False
+
+    def puede_crear_tarjeta_credito(self) -> bool:
+        return False
+
+    def puede_comprar_dolar(self) -> bool:
+        return False
+
+
+class ClienteBlack(Cliente):
+    def __init__(
+        self,
+        direccion: Direccion,
+        nombre: str,
+        apellido: str,
+        numero: str,
+        dni: str,
+    ) -> None:
+        cuentas: list[Cuenta] = []
+
+        caja_de_ahorro_en_pesos: Cuenta = Cuenta(
+            limite_extraccion_diario=100000,
+            limite_trasnferencia_recibida=LIMITE_MAXAXIMO_DE_TRANSFERENCIA,
+            monto=0,
+            costo_trasnferencia=0,
+            saldo_descubierto_disponible=10000,
+        )
+
+        cuenta_corriente: Cuenta = Cuenta(
+            limite_extraccion_diario=100000,
+            limite_trasnferencia_recibida=LIMITE_MAXAXIMO_DE_TRANSFERENCIA,
+            monto=0,
+            costo_trasnferencia=0,
+            saldo_descubierto_disponible=10000,
+        )
+
+        caja_de_ahorro_en_dolares: Cuenta = Cuenta(0, 0, 0, 0, 0,)
+
+        cuentas.append(caja_de_ahorro_en_pesos)
+
+        cuentas.append(cuenta_corriente)
+
+        cuentas.append(caja_de_ahorro_en_dolares)
+
+        super().__init__(
+            cuentas=cuentas,
+            direccion=direccion,
+            nombre=nombre,
+            apellido=apellido,
+            numero=numero,
+            dni=dni,
+        )
+
+    def puede_crear_chequera(self) -> bool:
+        return False
+
+    def puede_crear_tarjeta_credito(self) -> bool:
+        return False
+
+    def puede_comprar_dolar(self) -> bool:
+        return False
