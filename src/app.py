ba5757ff3ea6fbb0e6752fe48ce91bd370ce25e8
R. Nicolás Savinelli <savinelli@ieee.org>
2022-07-16 15:18:37 -0300

    integrate app.py to the new pkg structure

--- src/app.py
+++ src/app.py
@@ -1,20 +1,22 @@
-from clases import Parser
+from pkg.html import HtmlProcessor
+from pkg.buscador_problema import BuscadorProblema
+from pkg.parser import Parser
 
 if __name__ == "__main__":
-    parse = Parser()
-    cliente, eventos = parser.execute('eventos.json')
-    salida = ProcesadorHtml()
+    parser = Parser()
+    cliente, eventos = parser.execute('../resources/json/test.json')
+    salida = HtmlProcessor()
 
-    buscador = BuscadorProblema(cliente)
+    #buscador = BuscadorProblema(cliente)
     # podria crear una app parecida a la del profe
     print(cliente.nombre)
     print(cliente.apellido)
     print(cliente.dni)
-    print(cliente.cuenta.limite_extraccion_diario)
 
-for e in eventos:
-    #print("transaccion:", e.estado)
-    salida.append(buscador.getResultado(e))
-salida.crear_html(cliente)
+    #for e in eventos:
+        #print("transaccion:", e.estado)
+        #salida.append(buscador.getResultado(e))
+
+    salida.crear_html(cliente)
 
 #Deberiamos importar el buscador problema
