e147ef902d7023da787ea014f138c8a927751ec9
R. Nicolás Savinelli <savinelli@ieee.org>
2022-07-17 23:48:41 -0300

    feat: migrate app.py to implement the latest class definitions

--- src/app.py
+++ src/app.py
@@ -1,22 +1,18 @@
 from pkg.html import HtmlProcessor
-from pkg.buscador_problema import BuscadorProblema
 from pkg.parser import Parser
+import sys
 
 if __name__ == "__main__":
-    parser = Parser()
-    cliente, eventos = parser.execute('../resources/json/test.json')
-    salida = HtmlProcessor()
+    if len(sys.argv) >= 2:
+        htmlProcessor = HtmlProcessor()
 
-    #buscador = BuscadorProblema(cliente)
-    # podria crear una app parecida a la del profe
-    print(cliente.nombre)
-    print(cliente.apellido)
-    print(cliente.dni)
+        parser = Parser(target_json_file=sys.argv[1])
+        try:
+            parser.execute()
+        except Exception as error:
+            print("Failed to parse JSON")
+            raise(error)
 
-    #for e in eventos:
-        #print("transaccion:", e.estado)
-        #salida.append(buscador.getResultado(e))
-
-    salida.crear_html(cliente)
-
-#Deberiamos importar el buscador problema
+        htmlProcessor.crear_html(parser.cliente, parser.transacciones)
+    else:
+        print("Usage: python.exe app.py [JSON_FILE]")
