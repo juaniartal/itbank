0cd7c8e683cbfba2fba1bb527b687a53722600dc
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-08-15 00:21:18 -0300

    [Problematic-1] feat: add login mvt

--- homebanking/login/views.py
+++ homebanking/login/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    template_name = 'login/index.html'
+    context = {}
+    return render(request, template_name, context)
