d3a3d9e5ecba4504cdef1a43675cb5a68704fe6a
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-08-15 16:11:01 -0300

    [Problematic-2] feat: add costumer fields

--- homebanking/login/forms.py
+++ homebanking/login/forms.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.forms import TextInput, EmailInput, PasswordInput, CharField
+from django.forms import TextInput, EmailInput, PasswordInput, CharField, DecimalField, NumberInput
 
 from .models import Cliente
 
@@ -22,13 +22,32 @@ class RegisterForm(UserCreationForm):
         help_text="Enter the same password as before, for verification.",
     )
 
+    dni = DecimalField(
+        label="ID Number",
+        widget=NumberInput(attrs={"class": "form-control", "placeholder": '99.999.999'}),
+        min_value=0,
+        step_size=1,
+    )
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'dni']
         widgets = {
             'username': TextInput(attrs={
                 'class': 'form-control',
                 'placeholder': 'John.D0e',
+            }),
+            'first_name': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'John',
+                'maxlength': '150',
+                'required': 'true',
+            }),
+            'last_name': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Doe',
+                'maxlength': '150',
+                'required': 'true',
             }),
             'email': EmailInput(attrs={'class': 'form-control',
                                        'placeholder': 'your@mail.com',
@@ -38,6 +57,6 @@ class RegisterForm(UserCreationForm):
     def save_customer(self, commit=True):
         user = super().save(commit=commit)
         if commit:
-            costumer = Cliente(user=user)
+            costumer = Cliente(user=user, dni=self.cleaned_data['dni'])
             costumer.save()
         return user
