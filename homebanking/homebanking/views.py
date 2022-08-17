60a9914ae2bf721fc3aeec1ce366e2b2081dd71c
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-08-17 19:03:56 -0300

    [Problematic-1] refactor: home page with login and navbar

--- homebanking/homebanking/views.py
+++ homebanking/homebanking/views.py
@@ -1,14 +1,12 @@
-from django.contrib.auth.decorators import login_required
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
 from django.shortcuts import render
-
 
 
 def index(request: WSGIRequest) -> HttpResponse:
     """
     View function for Me Page of site.
     """
-    template_name: str = 'base.html'
+    template_name: str = 'index.html'
     context: dict = {}
     return render(request, template_name, context)
