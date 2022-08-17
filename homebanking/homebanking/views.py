c097e20a650e0c61d6485c8d3270a66cd204eb9a
PrimerCambio <camilanoemitisera@gmail.com>
2022-08-16 22:25:37 -0300

    render

---
+++ homebanking/homebanking/views.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.decorators import login_required
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+
+def index(request: WSGIRequest) -> HttpResponse:
+    """
+    View function for Me Page of site.
+    """
+    template_name: str = 'homebanking/index.html'
+    context: dict = {}
+    return render(request, template_name, context)
