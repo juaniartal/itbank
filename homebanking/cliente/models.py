584ffb39c84c365fe8f4ba43007c26d5aca77f86
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-08-17 23:50:43 -0300

    [Problematic-3] feat: add customer type

--- homebanking/cliente/models.py
+++ homebanking/cliente/models.py
@@ -4,9 +4,19 @@ from django.db import models
 
 # Create your models here.
 class Cliente(models.Model):
+    class CustomerType(models.TextChoices):
+        CLASSIC = 'C', 'Classic'
+        GOLD = 'G', 'Gold'
+        BLACK = 'B', 'Black'
+
     id = models.IntegerField(primary_key=True, editable=False)
     user = models.OneToOneField(User,
                                 on_delete=models.CASCADE, unique=True)
+    type = models.CharField(
+        max_length=1,
+        choices=CustomerType.choices,
+        default=CustomerType.CLASSIC,
+    )
     dni = models.IntegerField(null=True)
     dob = models.CharField(max_length=200, null=True)
     branch_id = models.IntegerField(null=True)
