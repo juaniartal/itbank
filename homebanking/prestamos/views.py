b51e91c22faece93cb9b7b052233f7995b56c8b7
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-08-18 20:02:40 -0300

    [Problematic-3] style: update loan view

--- homebanking/prestamos/views.py
+++ homebanking/prestamos/views.py
@@ -1,7 +1,9 @@
+import locale
+
 from django.contrib.auth.decorators import login_required
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from cliente.models import Cliente
@@ -10,8 +12,26 @@ from .models import Prestamo
 
 @login_required()
 def index(request: WSGIRequest) -> HttpResponse:
+    if request.method == 'POST':
+        return new_loan(request)
+    locale.setlocale(locale.LC_ALL, 'es_AR.UTF-8')
     user = request.user
-    costumer = Cliente.objects.get(user=user)
-    context: dict = {'user': user, 'costumer': costumer, 'max_loan': Prestamo.get_max_loan(costumer)}
+    customer = Cliente.objects.get(user=user)
+    max_loan = Prestamo.get_max_loan(customer)
+    loan_types = []
+    for loan_type in Prestamo.LoanType.choices:
+        loan_types.append({'value': loan_type[0], 'label': loan_type[1]})
+
+    context: dict = {'user': user,
+                     'customer': customer,
+                     'max_loan': max_loan,
+                     'loan_currency': locale.currency(max_loan, grouping=True),
+                     'loan_types': loan_types,
+                     }
     template_name: str = 'prestamos/prestamos.html'
     return render(request, template_name=template_name, context=context)
+
+
+@login_required()
+def new_loan(request: WSGIRequest) -> HttpResponse:
+    return redirect('loans')
