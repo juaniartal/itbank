6cf37e6ee5dd25177f60535247d6c9787aeb2b19
R. Nicolás Savinelli <savinelli@ieee.org>
2022-08-28 23:53:34 -0300

    implement serializers

--- api/homebanking/api/serializers.py
+++ api/homebanking/api/serializers.py
@@ -1,62 +1,69 @@
+from rest_framework import serializers
+from base.models import *
 
 
-class Cliente(serializers.HyperlinkedModelSerializer):
+class ClienteSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
-        read_only_fields = ("customer_id",)
+        read_only_fields = ("id",)
 
 
-class Cuenta(serializers.HyperlinkedModelSerializer):
+class ClienteTipoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Cliente.CustomerType
+        fields = '__all__'
+
+
+class CuentaSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Cuenta
         fields = '__all__'
 
 
-class Prestamo(serializers.HyperlinkedModelSerializer):
+class TipoCuentaSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Cuenta.AccountType
+        fields = '__all__'
+
+
+class PrestamoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Prestamo
         fields = '__all__'
 
 
-class Tarjeta(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Tarjeta
-        fields = '__all__'
-
-
-class Direccion(serializers.HyperlinkedModelSerializer):
+class DireccionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Address
         fields = '__all__'
-        read_only_fields = ("address_id",)
 
 
-class Sucursal(serializers.HyperlinkedModelSerializer):
+class SucursalSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Branch
         fields = '__all__'
 
 
-class ClienteTipo(serializers.HyperlinkedModelSerializer):
+class PrestamoTipoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Cliente
+        model = Prestamo.LoanType
         fields = '__all__'
 
 
-class PrestamoTipo(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Prestamo
-        fields = '__all__'
-
-
-class MarcaTarjeta(serializers.HyperlinkedModelSerializer):
+class TarjetaSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Tarjeta
         fields = '__all__'
 
 
-class TipoCuenta(serializers.HyperlinkedModelSerializer):
+class MarcaTarjetaSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Cuenta
+        model = Tarjeta.CardBrand
         fields = '__all__'
+
+
+class TipoTarjetaSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Tarjeta.CardType
+        fields = '__all__'        
