c6c72a50c08374c35acc92f77c2db30822b43c7e
R. Nicolás Savinelli <savinelli@ieee.org>
2022-08-29 21:17:37 -0300

    remove unnecessary code

--- api/homebanking/api/views.py
+++ api/homebanking/api/views.py
@@ -6,28 +6,24 @@ from base.models import *
 from .serializers import *
 
 class Clientes(APIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     def get(self, request, cliente_id):
         cliente = Cliente.objects.filter(id=cliente_id)
         serializer = ClienteSerializer(cliente, many=True, context={'request': request}) 
         return Response(serializer.data, status=status.HTTP_200_OK)    
 
 class Cuentas(APIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     def get(self, request, cliente_id):
         cuenta = Cuenta.objects.filter(customer_id=cliente_id).order_by('id')
         serializer = CuentaSerializer(cuenta, many=True, context={'request': request}) 
         return Response(serializer.data, status=status.HTTP_200_OK)    
 
 class Prestamos(APIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     def get(self, request, cliente_id):
         prestamo = Prestamo.objects.filter(customer_id=cliente_id).order_by('id')
         serializer = PrestamoSerializer(prestamo, many=True, context={'request': request}) 
         return Response(serializer.data, status=status.HTTP_200_OK)                    
 
 class PrestamosSucursal(APIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     def get(self, request, branch_id):
         prestamos = []
         clientes = Cliente.objects.filter(branch_id = branch_id).order_by('id')
@@ -37,14 +33,12 @@ class PrestamosSucursal(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)                    
 
 class Tarjetas(APIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     def get(self, request, cliente_id):
         tarjeta = Tarjeta.objects.filter(customer_id=cliente_id).order_by('id')
         serializer = TarjetaSerializer(tarjeta, many=True, context={'request': request}) 
         return Response(serializer.data, status=status.HTTP_200_OK)    
 
 class Sucursales(APIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     def get(self, request):
         sucursales = Branch.objects.all().order_by('id')
         serializer = SucursalSerializer(sucursales, many=True, context={'request': request}) 
